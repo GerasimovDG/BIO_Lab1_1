a75c1c1c63f8ccc03196ee6033d9c461fc6c2f71
GerasimovDG <demonmage24@gmail.com>
2017-11-21 15:08:48 +0300

    Initial commit

---
+++ BIO_Lab1/BIO_Lab1.cpp
@@ -0,0 +1,45 @@
+// BIO_Lab1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int NumberOfPattern(char* dnk, char* pattern)
+{
+	int tmp = 0;	// число вхождений
+	for (int i = 0; i < strlen(dnk); i++)
+	{
+		// сравнивает strlen(pattern) символов dnk с pattern 
+		if (strncmp((dnk + i), pattern, strlen(pattern)) == 0)
+			tmp++;
+	}
+	return tmp;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	ifstream fin("input.txt");
+	if (!fin.is_open()) // если файл не открыт
+		cout << "Файл не может быть открыт!\n"; // сообщить об этом
+	else
+	{
+		char dnk[80];
+		char *pattern = new char[80];
+		fin.getline(dnk, 80);		// считать 1 строку ( не больше 30 символов)
+		fin.getline(pattern,80);	// считать 2 строку
+		fin.close();
+		//cout << dnk << endl;
+		//cout << pattern << endl;
+		int tmp = NumberOfPattern(dnk, pattern);	// число вхождений
+		//cout << tmp << endl;
+		ofstream fout("output.txt");
+		fout << tmp;
+		fout.close();
+	}
+    return 0;
+}
+
